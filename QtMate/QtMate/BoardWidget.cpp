38407802560f5ad9ff5307f8f3380d010f8112eb
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2025-11-26 07:42:08 +0900

    0006 ◎ C 11/26 11/26 0.001│ToDo│BoardWidget：グリッド線描画

--- QtMate/QtMate/BoardWidget.cpp
+++ QtMate/QtMate/BoardWidget.cpp
@@ -18,4 +18,15 @@ void BoardWidget::paintEvent(QPaintEvent* event)
     painter.fillRect(QRect(0, 0, ww, wh), Qt::black);
     painter.fillRect(QRect(24, 24, 48*9, 48*9), QColor("#f0f0f0"));
 
+    for(int i = 1; i < N_HORZ; ++i) {
+        if (i % 3 == 0) {
+            pen.setWidth(4);                    // 線幅 px
+        } else {
+            pen.setWidth(1);                    // 線幅 px
+        }
+        painter.setPen(pen);
+        painter.drawLine(xyToPoint(0, i), xyToPoint(N_HORZ, i));
+	    painter.drawLine(xyToPoint(i, 0), xyToPoint(i, N_HORZ));
+    }
+
 }
--- QtMate/QtMate/BoardWidget.h
+++ QtMate/QtMate/BoardWidget.h
@@ -1,6 +1,10 @@
 ﻿#pragma once
 #include <QWidget>
 
+const int N_HORZ = 9;
+const int MARGIN = 24;
+const int CELL_WD = 48;
+
 class BoardWidget : public QWidget
 {
 	Q_OBJECT
@@ -9,6 +13,9 @@ public:
 	BoardWidget(QWidget *parent = nullptr) : QWidget(parent) {}
 
 protected:
+	QPoint		xyToPoint(int x, int y) const {
+		return QPoint(x*CELL_WD+MARGIN, y*CELL_WD+MARGIN);
+	}
     void paintEvent(QPaintEvent* event) override;
 };
 
