ba087ddca39b099ee9802b3ebaf07af022bde779
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2025-11-30 07:12:13 +0900

    0016 ◎ C 11/30 11/30 0.001│問題│BoardWidget：手番表示の石の下部が切れている

--- QtMate/QtMate/Board.cpp
+++ QtMate/QtMate/Board.cpp
@@ -1,2 +1,6 @@
 #include "Board.h"
 
+void Board::init() {
+	for(auto& v: m_localBoard) v = EMPTY;
+	for(auto& v: m_globalBoard) v = EMPTY;
+}
--- QtMate/QtMate/Board.h
+++ QtMate/QtMate/Board.h
@@ -1,6 +1,15 @@
 #pragma once
 #include <iostream>
 
+enum {
+	EMPTY = 0, BLACK, WHITE,
+};
+
+const int BOARD_WD = 3;
+const int BOARD_SIZE = BOARD_WD * BOARD_WD;
+const int BOARD9_WD = 9;
+const int BOARD9_SIZE = BOARD9_WD * BOARD9_WD;
+
 using Color = char;
 
 class Board
@@ -8,9 +17,15 @@ class Board
 public:
 	Board() {
 		std::cout << "Board::Board()" << std::endl;
+		init();
 	}
 	~Board() {
 		std::cout << "Board::~Board()" << std::endl;
 	}
+public:
+	void	init();
+private:
+	Color	m_localBoard[BOARD9_SIZE];
+	Color	m_globalBoard[BOARD_SIZE];
 };
 
